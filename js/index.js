b4f9ff78462494ff52525a1ca64c245a310b60f1
rpsPRO <rplazassanchez@gmail.com>
2021-03-21 23:38:33 +0100

    Versión final

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 //Arrancamos el controlador
 $.controller.init("panel_inicio");
+
 $('.navbar-nav li a').on('click',function(){
     if (!$(this).hasClass('dropdown-toggle')) {
         $('.navbar-collapse').collapse('hide');
