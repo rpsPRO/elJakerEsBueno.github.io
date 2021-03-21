82d4828569da849cda147fe4796796d9c05096e1
rpsPRO <rplazassanchez@gmail.com>
2021-03-21 22:54:45 +0100

    penúltimo commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 
 //Arrancamos el controlador
 $.controller.init("panel_inicio");
+$('.navbar-nav li a').on('click',function(){
+    if (!$(this).hasClass('dropdown-toggle')) {
+        $('.navbar-collapse').collapse('hide');
+    }
+});
